587b3ffd38cd6cf8e607914b7d94f47bfb23a35e
Marss616 <67536529+Marss616@users.noreply.github.com>
2023-06-07 14:11:25 +1000

    Framework for new buttons for V2

--- backend.py
+++ backend.py
@@ -5,7 +5,7 @@ import mysql.connector
 class ConnectSQL:
     def __init__(self, host, user, password):
         # Start connect to mysql
-        self.host = '192.168.116.155' 
+        self.host = '192.168.116.164' 
         self.user = 'jack' 
         self.password = 'User12345678!!' 
         self.cnx = mysql.connector.connect(host=host,user=user,password=password)
@@ -17,5 +17,5 @@ class ConnectSQL:
         rows = self.cursor.fetchall()
         return '\n'.join([', '.join(map(str, row)) for row in rows])
 
-sql_conn = ConnectSQL('192.168.116.155', 'jack', 'User12345678!!')
+sql_conn = ConnectSQL('192.168.116.164', 'jack', 'User12345678!!')
 
--- frontend.py
+++ frontend.py
@@ -4,7 +4,7 @@ import backend
 # creating main tkinter window/toplevel
 master = Tk()
 master.title("GUI SQL display Version 1.0")
-master.geometry('425x400') # Set window size
+master.geometry('500x400') # Set window size
 
 def call():
     command = sql_cmd_entry.get()
@@ -20,7 +20,9 @@ sql_cmd_entry = Entry(master)
 
 sql_cmd_button = Button(master, text="Enter SQL", command=call)
 sql_cmd_button_delete = Button(master, text="Delete output", command=delete)
-sql_cmd_show_databases_button = Button(master, text="test", command=call) # flag 1
+sql_cmd_show_databases_button0 = Button(master, text="test", command=call) # flag 0
+sql_cmd_show_databases_button1 = Button(master, text="test", command=call) # flag 1
+sql_cmd_show_databases_button2 = Button(master, text="test", command=call) # flag 2
 
 
 header_output = Label(master, pady=10, text = "Output: ", font=("Comic Sans MS", 20))
@@ -32,12 +34,14 @@ sql_cmd_output = Text(master, width=50, height=10)
 sql_cmd_label.grid(row = 1, column = 1, padx=20, pady=10)
 sql_cmd_entry.grid(row = 2, column = 1, pady=10,)
 
-sql_cmd_button.grid(row = 3, column = 1,)
-sql_cmd_show_databases_button.grid(row = 3, column = 2) # flag 1
+sql_cmd_button.grid(row = 3, column = 0)
+sql_cmd_show_databases_button0.grid(row = 3, column = 1) # flag 1
+sql_cmd_show_databases_button1.grid(row = 4, column = 1) # flag 2
+sql_cmd_show_databases_button2.grid(row = 4, column = 1) # flag 3
 
-sql_cmd_button_delete.grid(row = 4, column = 1, pady=10)
-header_output.grid(row = 5, column = 1)
-sql_cmd_output.grid(row = 6, column = 1)
+sql_cmd_button_delete.grid(row = 5, column = 1, pady=10)
+header_output.grid(row = 6, column = 1)
+sql_cmd_output.grid(row = 7, column = 1)
 
 
 # START Place Wigets
